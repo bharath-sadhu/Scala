05b9c156f970df8a4d3aa333a20450276815c3ef
bharath-sadhu <bharath.8199@gmail.com>
2017-07-13 15:11:01 +0530

    Delete Consumer.scala

--- DependencyInjection/Consumer.scala
+++
@@ -1,7 +0,0 @@
-package org.scala.dependencyInjection
-
-class Consumer(messageService: MessageService) {
-  def processMessage(message: String, reciever: String) {
-    messageService.sendMessage(message, reciever)
-  }
-}

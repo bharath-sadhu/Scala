4d85d8dea4f9f47707d0726108cc3ab8d061fc2c
bharath-sadhu <bharath.8199@gmail.com>
2017-07-13 15:11:08 +0530

    Delete EmailService.scala

--- DependencyInjection/EmailService.scala
+++
@@ -1,7 +0,0 @@
-package org.scala.dependencyInjection
-
-class EmailService extends MessageService {
-  def sendMessage(email: String, reciever: String) {
-    print("Send " + email + " to " + reciever)
-  }
-}

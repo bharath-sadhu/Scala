ecd64389d3ac5264ac34900cc481814128cc7659
bharath-sadhu <bharath.8199@gmail.com>
2017-07-13 15:11:41 +0530

    Delete SmsService.scala

--- DependencyInjection/SmsService.scala
+++
@@ -1,7 +0,0 @@
-package org.scala.dependencyInjection
-
-class SmsService extends MessageService {
-  def sendMessage(sms: String, reciever: String) {
-    print("send" + sms + "to" + reciever)
-  }
-}

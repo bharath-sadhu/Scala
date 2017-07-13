68864f445f561d7df0529a5ab825fc4606bd2fb5
bharath-sadhu <bharath.8199@gmail.com>
2017-07-13 15:10:48 +0530

    Delete SendMessageApp.scala

--- DependencyInjection/SendMessageApp.scala
+++
@@ -1,8 +0,0 @@
-package org.scala.dependencyInjection
-
-object SendMessageApp {
-  def main(args: Array[String]): Unit = {
-    val injector: ServiceInjector = new EmailServiceInjector()
-    injector.getConsumer().processMessage("Hi how are you", "bharath.8199@gmail.com")
-  }
-}
